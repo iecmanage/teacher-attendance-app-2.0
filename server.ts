a6419424686e08fc3234912edbfa6e95ca7a69e6
iecmanage <qari.iec05@gmail.com>
2026-08-05 19:02:32 +0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -11,7 +11,8 @@ import {
 const app = express();
 const PORT = 3000;
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const STORE_PATH = path.join(process.cwd(), 'data_store.json');
 
